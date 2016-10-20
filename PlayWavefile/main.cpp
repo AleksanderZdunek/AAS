ee5e2d6ff06faf1ec9ea0739940a6904d8b2604b
Aleksander Zdunek <halekun@gmail.com>
2016-10-20 21:32:46 +0200

    Play file from command line argument

--- PlayWavefile/main.cpp
+++ PlayWavefile/main.cpp
@@ -13,13 +13,24 @@ int main(int argc, char* argv[])
 {
 	std::cout << "Play Wavefile!" << std::endl;
 
+	char defaultFilename[]{ "" };
+	char* filename = 1 < argc ? argv[1] : defaultFilename;
+	std::cout << filename << std::endl;
+
 	Header h;
 	ChunkHeader tmpChunkHeader;
 	WAVEFORMATEXTENSIBLE formatChunk{0};
 
 	std::ifstream file;
 	file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-	file.open("No Doubt - Tragic Kingdom.wav", std::ifstream::binary);
+	try {
+		file.open(filename, std::ifstream::binary);
+	}catch(std::exception e)
+	{
+		std::cerr << "Error on opening file" << std::endl;
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
 
 	file.read((char*)&h, 12);
 
