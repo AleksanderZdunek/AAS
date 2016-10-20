39cbb43fb455215e366cda25973f1c7773f0f58d
Aleksander Zdunek <halekun@gmail.com>
2016-10-20 21:06:47 +0200

    Removed old and unused files

--- PlayWavefile/archieutil.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifndef ARCHIEUTIL_H
-#define ARCHIEUTIL_H
-#include  <cstdint>
-
-void samplerateConverter(uint32_t target_rate, uint32_t source_rate, uint32_t blockSize, char* target, char* source, size_t target_size, size_t source_size);
-
-#endif // !ARCHIEUTIL_H
