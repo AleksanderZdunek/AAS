432476685c53b65dc53fdfe4fd63e294227c7cda
Aleksander Zdunek <halekun@gmail.com>
2016-11-18 19:52:59 +0100

    Added new separate Archie lib project. Copied archie files from PlayWavefile project.

---
+++ Archie/archie.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef ARCHIE_H
+#define ARCHIE_H
+#include <functional>
+#include <Audioclient.h>
+
+namespace Archie{
+
+extern WAVEFORMATEXTENSIBLE configFormat;
+extern std::function<DWORD(UINT32, BYTE*)> LoadData;
+
+bool Init();
+void UnInit();
+
+void Play();
+
+}//!namespace Archie
+
+#endif //!ARCHIE_H
